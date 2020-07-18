b56f75bcf6b8e6d5b819fdae96eaf229586a6f7e
chetan <chetansharma.019@gmail.com>
2020-07-18 23:59:11 +0200

    comments in bubble sort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -5,9 +5,19 @@ function swapArrayElem(arr, pos1, pos2) {
   return arr;
 }
 
+
+/** 
+ * The essence of bubble sort is in swapping the two adjacent values,
+ * smaller to the left and greater to the right. 
+ * This is done starting from left most side.
+*/
 function bubbleSort(arr) {
-  for (let outer = arr.length; outer > 1; outer--) {
-    for (let inner = 0; inner <= outer - 1; inner++) {
+  // each iteration will reduce on node to inspect.
+  for (let nodesToInspect = arr.length; nodesToInspect > 1; nodesToInspect--) {
+    // start comparison from 0th element and go till the remaining nodeToInspect.
+    // since the nodeToInspect will be reached by the "next" element inner can remain 
+    // <= nodesToInspect - 1
+    for (let inner = 0; inner <= nodesToInspect - 1; inner++) {
       let currElem = arr[inner];
       let nextElem = arr[inner + 1];
       if (currElem > nextElem) {
