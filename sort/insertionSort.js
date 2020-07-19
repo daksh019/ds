7991e88af2c09376dee0c65697f8e90f0b088b99
chetan <chetansharma.019@gmail.com>
2020-07-19 19:36:04 +0200

    insertion sort with inplace changes

--- sort/insertionSort.js
+++ sort/insertionSort.js
@@ -6,6 +6,32 @@ function swapArrayElem(arr, pos1, pos2) {
 }
 
 
+/**
+ * Each element in the list is compared to the rest of the sub array in left direction.
+ * The comparison begins from second element and compares to first element. 
+ * Then moves to third elment to compare to first two elements and so on.
+ * Calling this element as "pegged" element.
+ * Inside each iteration comparison is made between "pegged" element value to "pegged" -n position.
+ * Until a value less than "pegged" element is found or the comparison reaches zeroth place.
+ * If the "pegged" element is lower than the nth element then the left side element is moved to right.
+ * The comparison moves to next set one. 
+ * In the end, the pegged element is placed at the nth place where this inner comparison loop breaks.
+*/
+function insertionSortInPlace(arr) { 
+  let totalIterations = 0;
+  for (let peggedPosition = 1; peggedPosition <= arr.length - 1; peggedPosition++) {
+    let peggedValue = arr[peggedPosition];
+    let comparePos = peggedPosition - 1;
+    while (comparePos > 0 && arr[comparePos - 1] >= peggedValue) { 
+      arr[comparePos] = arr[comparePos - 1];
+      comparePos--;
+      totalIterations++;
+    }
+    arr[comparePos] = peggedValue;
+  }
+  console.log("total iterations for inplace operation", totalIterations);
+}
+
 /** 
  * This one is shift based sort. 
  * Each element in the list is compared to the first pos and if this element is smaller then 
@@ -15,6 +41,7 @@ function swapArrayElem(arr, pos1, pos2) {
  * And each iteration will compare from iter element to last.
 */
 function insertionSort(arr) {
+  let totalIterations = 0;
   for (let outerIndex = 0; outerIndex < arr.length - 1; outerIndex++) {
     for (
       let innerIndex = outerIndex + 1;
@@ -26,39 +53,24 @@ function insertionSort(arr) {
         arr.splice(innerIndex, 1);
         arr.splice(outerIndex, 0, elemToRemove);
       }
+      totalIterations++;
       // console.log(arr);
     }
     console.log(arr);
   }
+  console.log("total iterations for shift operation", totalIterations);
 }
 
-// const arr = [3, 89, 67, 78, 79, 34, 99, 1, 99];
-const arr = [6,10,0,6,5,8,7,4,2,7];
+let arr = [6,10,0,6,5,8,7,4,2,7];
 insertionSort(arr);
 
 console.log("sorted result: ");
 console.log(arr);
 console.log("\n\n");
 
-function insertionSort2(dataStore) {
-  var temp, inner;
-  for (var outer = 1; outer <= dataStore.length-1; ++outer) {
-    temp = dataStore[outer];
-    inner = outer;
-    console.log("for outer as", outer, "temp is", temp);
-    while (inner > 0 && (dataStore[inner-1] >= temp)) {
-      dataStore[inner] = dataStore[inner-1];
-      --inner;
-      console.log(dataStore);
-    }
-    console.log("broke out of while, the inner is", inner);
-    dataStore[inner] = temp;
-    console.log(dataStore)
-  }
-}
+arr = [6,10,0,6,5,8,7,4,2,7];
 
-const arr2 = [6,10,0,6,5,8,7,4,2,7];
-insertionSort2(arr2);
-
+insertionSortInPlace(arr);
 console.log("sorted result: ");
-console.log(arr2);
+console.log(arr);
+// conclusion - inplace method is way cheaper than the shift on each detect.
